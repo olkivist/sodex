52ae1ee64f6579e3773e291f6fc3429ff63b5f1e
bandidoLando <opikivisto@gmail.com>
2017-03-29 15:19:42 +0300

    added db query with argument

--- app/Controllers/DatabaseController.php
+++ app/Controllers/DatabaseController.php
@@ -14,4 +14,25 @@ class DatabaseController extends BaseController {
 
 	}
 
+	// Anna osoiteriville parametrina ruokalaji 
+	public function findCourse($request, $response, $args) {
+
+		$course = $this->c->db->prepare("SELECT * FROM courses WHERE course_name = :course");
+
+		$course->execute([
+
+			'course' => $args['course']
+		]);
+
+		$course = $course->fetch(PDO::FETCH_OBJ);
+
+		var_dump($course);
+
+
+		// tee oma view courseille
+//		return $this->view->render($response, 'courses/course.twig', [
+//			'course' => $course_name
+//		]);
+	}
+
 }
