24f60e61990c66b8fea9ebaf7699c0b53b938ce0
bandidoLando <opikivisto@gmail.com>
2017-03-30 22:26:21 +0300

    layouts improved a bit

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,10 @@ use App\Controllers\CategoryController;
 	$app->get('/', function($request, $response) {
 
 		return $this->view->render($response, 'home.twig');
-	});
+	})->setName('home');
 
-	$app->get('/courses', CourseController::class . ':courses');
+	$app->get('/courses', CourseController::class . ':courses')->setName('courses');
 	$app->get('/courses/{course}', CourseController::class .':show')->setName('details.show');
 
-	$app->get('/categories', CategoryController::class .':categories');
+	$app->get('/categories', CategoryController::class .':categories')->setName('categories');
 	$app->get('/categories/{category}', CategoryController::class .':show')->setName('courses.show');
